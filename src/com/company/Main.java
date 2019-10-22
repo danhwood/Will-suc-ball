c943aabd13d58ba4a33cdfe53494a9d99fa5398e
Student   HARWOOD Daniel <92472@sfcf.local>
2019-10-22 13:57:32 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 1;
+	    while (x == 1) {
+	        System.out.println("Damn the sucks");
+        }
+    }
+}
